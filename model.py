f67ca33ea31f61119e2e218032aad48ed605fb40
emanuelthiago06 <emanuel.thiago@academico.ifpb.edu.br>
2023-09-15 18:50:06 -0300

    add support for keras tunning

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
-from keras import layers, models
+from keras import layers, models, metrics
+import keras
 
 
 def build_model(input_shape : tuple) -> models.Sequential:
@@ -15,4 +16,37 @@ def build_model(input_shape : tuple) -> models.Sequential:
                 loss='binary_crossentropy',
                 metrics=['accuracy'])
     model.summary()
+    return model
+
+def make_model(units, activation, dropout, lr, input_shape):
+    METRICS = [
+      keras.metrics.SensitivityAtSpecificity(name='Sen', specificity= 0.5),
+      keras.metrics.SpecificityAtSensitivity(name='Spe', sensitivity = 0.5),
+      keras.metrics.BinaryAccuracy(name='Acc'),
+      keras.metrics.AUC(name='AUC')
+    ]
+
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(1, activation='sigmoid'))
+    model.compile(optimizer='adam',
+                loss='binary_crossentropy',
+                metrics=['accuracy'])
+    model.summary()
+    return model
+
+def build_model_alt(hp):
+    units = hp.Int("units", min_value=32, max_value=512, step=32)
+    activation = hp.Choice("activation", ["relu", "tanh"])
+    dropout = hp.Boolean("dropout")
+    lr = hp.Float("lr", min_value=1e-4, max_value=1e-2, sampling="log")
+    # call existing model-building code with the hyperparameter values.
+    model = make_model(
+        units=units, activation=activation, dropout=dropout, lr=lr
+    )
     return model
